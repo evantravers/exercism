fb61ea2ffa0e392d113649785b17c371c547d61f
Evan Travers <evantravers@gmail.com>
2018-10-03 12:36:37 -0500

    Add `defp` to satisfy mentor

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -7,23 +7,23 @@ defmodule Roman do
     numerals(number, "")
   end
 
-  def numerals(0, roman), do: roman
-  def numerals(num, roman) do
+  defp numerals(0, roman), do: roman
+  defp numerals(num, roman) do
     {new_letter, to_subtract} = convert(num)
     numerals(num - to_subtract, roman <> new_letter)
   end
 
-  def convert(n) when n >= 1000, do: {"M", 1000}
-  def convert(n) when n >= 900, do: {"CM", 900}
-  def convert(n) when n >= 500, do: {"D", 500}
-  def convert(n) when n >= 400, do: {"CD", 400}
-  def convert(n) when n >= 100, do: {"C", 100}
-  def convert(n) when n >= 90, do: {"XC", 90}
-  def convert(n) when n >= 50, do: {"L", 50}
-  def convert(n) when n >= 40, do: {"XL", 40}
-  def convert(n) when n >= 10, do: {"X", 10}
-  def convert(n) when n >= 9, do: {"IX", 9}
-  def convert(n) when n >= 5, do: {"V", 5}
-  def convert(n) when n >= 4, do: {"IV", 4}
-  def convert(n) when n >= 1, do: {"I", 1}
+  defp convert(n) when n >= 1000, do: {"M", 1000}
+  defp convert(n) when n >= 900, do: {"CM", 900}
+  defp convert(n) when n >= 500, do: {"D", 500}
+  defp convert(n) when n >= 400, do: {"CD", 400}
+  defp convert(n) when n >= 100, do: {"C", 100}
+  defp convert(n) when n >= 90, do: {"XC", 90}
+  defp convert(n) when n >= 50, do: {"L", 50}
+  defp convert(n) when n >= 40, do: {"XL", 40}
+  defp convert(n) when n >= 10, do: {"X", 10}
+  defp convert(n) when n >= 9, do: {"IX", 9}
+  defp convert(n) when n >= 5, do: {"V", 5}
+  defp convert(n) when n >= 4, do: {"IV", 4}
+  defp convert(n) when n >= 1, do: {"I", 1}
 end
